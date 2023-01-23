641a731abd42d21b35cb7ec80dc984e891813c0c
mattia.ripamonti <mattia.ripamonti@bitrock.it>
2023-01-23 09:02:32 +0100

    add site

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '@/styles/globals.css'
+import '@/styles/bootstrap.css'
+import '@/styles/responsive.css'
+import '@/styles/style.scss'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
